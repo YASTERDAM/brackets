db6c0b3fd0a09e7c3121c95e9ddc5c04759ab261
Ivan Chelyadinskiy <ivan.drozd26062606@gmail.com>
2020-10-10 16:46:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig) { 
+  const config = new Map(bracketsConfig);
+  const st = []; 
+  for (let i in str) 
+    st.length > 0 && config.get(st[st.length - 1]) === str[i] ? st.pop() : st.push(str[i]); 
+  return st.length === 0; 
+};
